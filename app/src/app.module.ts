6dfc1cd994488b624b14dca64aa7e78b139c8af2
RomanG <roman29g@gmail.com>
2024-08-07 00:29:51 -0500

    fix: removed commented code

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -5,12 +5,9 @@ import { NotificationController } from './controllers/notification.controller';
 import { PricingController } from './controllers/pricing.controller';
 import { ProfileController } from './controllers/profile.controller';
 import { VolunteerController } from './controllers/volunteer.controller';
-
-import { HistoryController } from './controllers/history.controller';
-
 import { HomeController } from './controllers/home.controller';
 import { PublicController } from './controllers/public.controller';
-
+import { HistoryController } from './controllers/history.controller';
 
 import { AuthService } from './services/auth.service';
 import { EventService } from './services/event.service';
@@ -18,11 +15,8 @@ import { NotificationService } from './services/notification.service';
 import { PricingService } from './services/pricing.service';
 import { ProfileService } from './services/profile.service';
 import { VolunteerService } from './services/volunteer.service';
-
-import { HistoryService } from './services/history.service';
-
-
 import { SupabaseService } from './libs/db/supabase.service';
+import { HistoryService } from './services/history.service';
 
 @Module({
 
